5de906ba0e68b8579410f4fdefe7ec332ed0f2c5
badl7 <btlgrbz071@gmail.com>
2021-10-04 12:14:04 +0300

    denemeler

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from selenium import webdriver
 driver = webdriver.Firefox()
 
 # Navigate to page
-driver.get('http://besimgurbuz.com')
-print(driver.title)
+driver.get('http://besimgurbuz.com/software')
+
+elements = driver.find_elements_by_css_selector('.icons')
+for element in elements:
+    print("**************************************")
+    print(element.text)
 
 driver.quit()
